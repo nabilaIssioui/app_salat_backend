6654708bc6164102b3e94b0bde6f65e3dd115f15
unknown <aissiouinabila@gmail.com>
2024-11-29 17:05:45 +0100

    Sécuriser mon api adhan

--- routes/api.php
+++ routes/api.php
@@ -15,4 +15,4 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------|
 */
 
-    Route::get('getPrayers', [PrayerController::class, 'getPrayers'])->name('getPrayers');
+Route::get('/prayer-times', [PrayerController::class, 'getPrayerTimes']);
